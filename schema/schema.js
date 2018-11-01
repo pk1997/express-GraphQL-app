99898561a67a313ddd9ba0445564f16fa16c4241
pk1997 <skpavan2@gmail.com>
2018-11-01 10:47:18 +0530

    integrated axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,11 @@
 const graphql= require ('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const{
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
     GraphQLSchema
 }=graphql;
-const users = [
-    {   id:'23',firstName:'Bill',age:20},
-    {   id:'24',firstName:'George',age:20}
-]
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -26,7 +22,8 @@ const UserType = new GraphQLObjectType({
              type:UserType,
              args:{id:{type:GraphQLString}},
              resolve(parentValue,args){
-                return _.find(users,{id:args.id}); 
+                return axios.get(`http://localhost:3000/users/${args.id}`) 
+                .then(response => response.data).then(data=>data)
              }
          }
 
